0583c3a50a246c642f154a76146ff56e024983c8
MuhammadqodirProgrammer <komilovm831@gmail.com>
2022-12-24 17:36:22 +0500

    first commit

---
+++ js/corusel.js
@@ -0,0 +1,57 @@
+$('.marketing__colusel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $('.marketing__colusel-mobile').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+//   $('.partnyor__corusel_toleft').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+
+//   });
+
+
+
+  $('.partnyor__corusel').slick({
+    autoplay:true,
+    infinite:true,
+    autoplaySpeed:0,
+    slidesToScroll:1,
+    slidesToShow:5,
+    arrows:false,
+    cssEase:'linear',
+    speed:1000,
+    initialSlide:1,
+    draggable:false,
+     rtl:true,
+  });
+  
+  $('.partnyor__corusel_toleft').slick({
+    autoplay:true,
+    infinite:true,
+    autoplaySpeed:0,
+    slidesToScroll:1,
+    slidesToShow:5,
+    arrows:false,
+    cssEase:'linear',
+    speed:1000,
+    initialSlide:1,
+    draggable:false,
+    //  rtl:true,
+  });
+//   $('.partnyor__corusel').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//      rtl:true,
+//   });
